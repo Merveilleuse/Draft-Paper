e586df910561fd5d46b5cdf862a11b14a99f2083
Merveilleuse-UCT <mbnmar005@myuct.ac.za>
2017-09-14 23:02:10 +0200

    final draft with all the relevant code and output data. Note than running the R code (rolling period code.R) takes some time and so the esults where stored as text files names "h step ahead results" for h=1,5,20

---
+++ Texevier-master/Readme.md
@@ -0,0 +1,45 @@
+Introduction
+============
+
+This package helps a user set up an Elsevier template for writing academic journal reports in Rmarkdown. The setup requires very few lines of code and will get you up and running in *a few seconds* (provided you have all the relevent dependencies installed - see below), with a fully working and good-to-go template.
+
+Using Texevier you can simply start writing your paper / thesis / phd in R, with a template guiding you through the most important aspects you will encounter in your writing. Your template will look as follows:
+
+![Screenshot](inst/ScreenShot/Example.PNG)
+
+Dependencies Required
+---------------------
+
+Make sure you have the following installed on your computer:
+
+-   [R](http://cran.r-project.org/bin/windows/base/)
+-   [Rstudio](http://www.rstudio.com/)
+
+Optional (Should be installed automatically)
+--------------------------------------------
+
+-   [MikTex](http://miktex.org/download)
+
+Code
+----
+
+After installing all three at the top, simply run the following code:
+
+    if (!require("devtools")) install.packages("devtools")
+    library(devtools)
+    devtools::install_github("Nicktz/Texevier")
+    library(Texevier)
+    # Input your directory as dir:
+    dir <- "YOUR DIRECTORY HERE"
+    create_template(directory = dir, template_name = "Template", launch_template = TRUE, ShowPDFatLaunch = TRUE)
+
+This will automatically install all the needed files and components, and allow you to start working with an Elsevier template in less than a minute. A pdf will pop up, if you keep ShowPDFatLaunch = TRUE, which you can scroll through to see what your template looks like. REMEMBER to close the pdf before attempting to edit the template. Conversely, set ShowPDFatLaunch = FALSE. By default your template R files will be launched. To only create the folder, set launch\_template = FALSE.
+
+A template.Rmd file will also be opened automatically from the directory provided, which contains all the needed notes on how to write an academic paper in R to produce the pdf example shown. I recommend going through the code in depth for a good reference before you start working. Also, start your paper journey by creating a R.proj within your working folder.
+
+Motivation
+----------
+
+Doing research in R allows you to verify your work easily. This template is designed to save the researcher time in terms of setting up a proper template for doing formal research. This is something which could feel daunting and can be time-consuming for even intermediate LaTeX and R users. This package is intended to get you started in less than a minute.
+
+Feel free to edit the templates and use this as a starting point for your own research.
---
+++ rolling period code.R
@@ -0,0 +1,283 @@
+#2017 Honours Project: Draft Code           **
+#Author: Marvelous Mubenesha                **  
+#Student No. MBNMAR005                      **
+#*********************************************
+#*********************************************
+
+
+#--------- i) Import, clean and transform data-----------------------------------
+
+library(readxl)
+top40index <- read_excel("~/Honours_project/top40index.xlsx", 
+                         col_types = c("date", "numeric"))
+
+#Data cleaning : removing duplicates
+index <- top40index[1:1827,]  #warning message is because data is being duplicated repeatedly. Duplicates start after row 1827
+attach(index)
+#head(index) 
+
+#Calculating log returns from price index
+library(PerformanceAnalytics)
+log.returns <- CalculateReturns(as.ts(index[,2]), method = "log")[-1][61:1826]  #reduced data set because it was taking too long to run rolling window forecasts
+
+
+
+#-------- ii) Exploratiory Data Analysis --------------------------------------
+
+#Basic plots for price index and log returns ----------------------
+plot(index, type = "l",
+     main = "FTSE/JSE Top40 Index 07/2010 - 04/2017")
+
+par(mfrow = c(1,2))
+plot(log.returns, type = "l", main = "Log return on Price Index 07/2010 - 07/2017")
+plot(density(log.returns, adjust = 3), main = "Distribution of Log-Returns on Price Index 07/2010 - 04/2017", col = 1, lty = 1) 
+par(mfrow = c(1,1))
+
+#Outliers
+boxplot(returns, log.returns, names = c("returns","log returns"), main = "Boxplots of Returns and Log Returns on full sample")  #Comment : looks like there are some outliers eventhough outliers are 'relatively' clustered
+
+#Formal tests for Outliers in Time series --------------------------
+
+library(tsoutliers)  # conducts multiple tests for outliers #Reference Chen and Liu;s as authors : https://stats.stackexchange.com/questions/104882/detecting-outliers-in-time-series-ls-ao-tc-using-tsoutliers-package-in-r-how
+library(ggpubr)
+log.returns.outlier.detection <- tsoutliers::tso(as.ts(log.returns),types = c("AO","IO","LS","TC","SLS"), maxit.iloop = 10)
+log.return.outlier.detection  #Several outliers detected
+#Further investigation shows that these outliers occur when return large in abs value. See : log.returns[c(299,302,348,1144,1156,1540)]
+
+
+
+
+#-------- iii) ARIMA MODEL IDENTIFICATION : BOX-JENKINS METHODOLOGY -----------------------
+
+#Partitioning data into in-sample data
+in.samp.log.returns <- log.returns[1:1400]  #Code took too long to run. So we looked at 5 year period 
+
+#   Step 1: Formally testing stationarity/Unit root tests
+library(tseries)   #for Augmented Dickey-Fuller test
+adf.test(in.samp.log.returns) # p < 0.01 therefore we reject the null. Data is stationary 
+
+#    Step 2: ACF and PACF plots for model identification
+par(mfrow = c(1,2))
+acf(in.samp.log.returns, main = "Autocorrelation Function for Log Returns")  #ma(2)
+pacf(in.samp.log.returns, main = "Partial Autocorrelation Function for Log Returns")  #ar(2), ar(30)
+par(mfrow = c(1,1))
+#ma and ar(30) could be overfitting and since model is for forecasting, I want it to be parsimonious ->
+#looks like there could be seasonality but its below the bandwith 
+
+#    Step 3: Fitting possible ARIMA models on log returns (model 1 - model 9)
+
+m1 <- arima(in.samp.log.returns, order = c(0,0,2))
+m2 <- arima(in.samp.log.returns, order = c(2,0,0))
+m3 <- arima(in.samp.log.returns, order = c(2,0,2))
+m4 <- arima(in.samp.log.returns, order = c(0,0,0))
+
+
+m1$aic; m2$aic; m3$aic; m4$aic
+
+# Model with the lowest AIC is m5 ARIMA(2,0,2)
+
+#Verfiying ARIMA Modelling using forecast package: automatically chooses lowest AIC model
+#library(forecast)
+#auto.arima(in.samp.log.returns, approximation = F, trace = F) #This also builds the best ARIMA model and it turns out that you got it right. Its the arima(2,0,2) :)
+
+
+#-------- iv) Rolling Window Forecasting Errors-------------------------------
+
+#Create dataframe compatible with prophet -----------------------
+library(prophet)
+library(bizdays)
+
+#Sequaence of trading dates
+start_date <- as.Date("2012-07-30")
+end_date <- as.Date("2017-05-30")   
+dates <- bizseq(start_date, end_date, "actual")
+prophet_data <- data.frame(ds = dates,y = log.returns[61:1826])   #do this for returns only if it runs smoothly and you have time
+
+#Convert Prophet returns data to xts format for ARIMA rolling period forecasting
+library(xts)
+toDate <- function(x) as.Date(x, origin = "2017-07-30")
+zoo.object <- read.zoo(prophet_data,header=TRUE,FUN=toDate)
+returns <- as.xts(zoo.object)  #returns data in xts format but dates include weekends. Prophet has hover matched days correctly so its not a big deal
+
+#Create out of sample period sequence
+out_of_sample <- index(returns)[index(returns) >= "2016-05-01"] #all dates after and incl. 1 May 2016
+
+
+# t+1 rolling period forecasts ------------------------------------
+fore_res_1 <- list()  #Initialize list to store results
+h=1
+
+#for each element in the out of sample period, calculate h step ahead forecast errors using a rolling window
+for(i in 1:(length(out_of_sample) - h)){
+  model_1 <- arima(returns[paste0('/', out_of_sample[i])], c(2,0,2),optim.control = list(maxit=1000))  #estimate ARIMA(2,0,2) with data in xts format
+  model_2 <-prophet(df=prophet_data[1:4000+i,])      #dataframe format supported by prophet
+  
+  #Evaluate h step ahead forecast at i for both models
+  fore_model_1 <- forecast(model_1, h = h)$mean %>% as.numeric %>% tail(1) 
+  fore_model_2 <- predict(model_2, make_future_dataframe(model_2, periods = h), include_history = FALSE)$yhat%>%tail(1)  #This takes >=10 seconds to run
+  
+  #Actual obs
+  actual <- returns[out_of_sample[i + h]] %>% as.numeric
+  
+  #Store results for each iteration
+  fore_res_1[[i]] <- data.frame(date = out_of_sample[i],   
+                                actual = actual,
+                                fore_model_1, 
+                                fore_model_2,
+                                error1 = actual - fore_model_1,
+                                error2 = actual - fore_model_2)
+  
+}
+fore_res_1 <- do.call(rbind, fore_res_1)
+
+# t+5 rolling period forecasts -----------------------------------------------
+fore_res_5 <- list()  #Initialize list to store results
+h=5
+
+#for each element in the out of sample period, calculate h step ahead forecast errors using a rolling window
+for(i in 1:(length(out_of_sample) - h)){
+  model_1 <- arima(returns[paste0('/', out_of_sample[i])], c(2,0,2),optim.control = list(maxit=1000))  #estimate ARIMA(2,0,2) with data in xts format
+  model_2 <-prophet(df=prophet_data[1:4000+i,])      #dataframe format supported by prophet
+  
+  #Evaluate h step ahead forecast at i for both models
+  fore_model_1 <- forecast(model_1, h = h)$mean %>% as.numeric %>% tail(1) 
+  fore_model_2 <- predict(model_2, make_future_dataframe(model_2, periods = h), include_history = FALSE)$yhat%>%tail(1)  #This takes >=10 seconds to run
+  
+  #Actual obs
+  actual <- returns[out_of_sample[i + h]] %>% as.numeric
+  
+  #Store results for each iteration
+  fore_res_5[[i]] <- data.frame(date = out_of_sample[i],   
+                                actual = actual,
+                                fore_model_1, 
+                                fore_model_2,
+                                error1 = actual - fore_model_1,
+                                error2 = actual - fore_model_2)
+  
+}
+fore_res_5 <- do.call(rbind, fore_res_5)
+
+
+
+
+
+
+# t+20 rolling period forecasts -------------------------------------------------------
+
+fore_res_20 <- list()  #Initialize list to store results
+h=20
+
+#for each element in the out of sample period, calculate h step ahead forecast errors using a rolling window
+for(i in 1:(length(out_of_sample) - h)){
+  model_1 <- arima(returns[paste0('/', out_of_sample[i])], c(2,0,2),optim.control = list(maxit=1000))  #estimate ARIMA(2,0,2) with data in xts format
+  model_2 <-prophet(df=prophet_data[1:4000+i,])      #dataframe format supported by prophet
+  
+  #Evaluate h step ahead forecast at i for both models
+  fore_model_1 <- forecast(model_1, h = h)$mean %>% as.numeric %>% tail(1) 
+  fore_model_2 <- predict(model_2, make_future_dataframe(model_2, periods = h), include_history = FALSE)$yhat%>%tail(1)  #This takes >=10 seconds to run
+  
+  #Actual obs
+  actual <- returns[out_of_sample[i + h]] %>% as.numeric
+  
+  #Store results for each iteration
+  fore_res_20[[i]] <- data.frame(date = out_of_sample[i],   
+                              actual = actual,
+                              fore_model_1, 
+                              fore_model_2,
+                              error1 = actual - fore_model_1,
+                              error2 = actual - fore_model_2)
+  
+}
+fore_res_20 <- do.call(rbind, fore_res_20)
+write.csv(fore_res_20,file="20 step ahead results")
+
+#Export results to text files (The rolling period approach took hours to run. Therefore, results were stored as a loss mitigation measure)
+write.table(fore_res_1,file="1 step ahead results.txt")
+write.table(fore_res_5,file="5 step ahead results.txt")
+write.table(fore_res_20,file="20 step ahead results.txt")
+
+# Forecast Error Statistics --------------------------------------------
+library(forecast)
+
+#Visual analysis
+
+boxplot(fore_res_1$error1, fore_res_1$error2,
+        fore_res_5$error1,fore_res_5$error2,
+        fore_res_20$error1,fore_res_20$error2,
+        main = "Boxplot of Rolling Period Forecast Errors for Arima and Prophet Models",
+        names = c("Arima(h = 1)","Prophet(h = 1)","Arima(h = 5)","Prophet(h = 5)","Arima(h = 20)","Prophet(h = 20)"))
+
+#Boxplot of forecast errors with ggplot2----
+library(ggplot2)
+
+x.1 <- rep(1, times = 365)
+x.2 <- rep(2, times = 365)
+x.3 <- rep(3, times = 361)
+x.4 <- rep(4, times = 361)
+x.5 <- rep(5, times = 346)
+x.6 <- rep(6, times = 346)
+x.fact <- c(x.1,x.2,x.3,x.4,x.5,x.6)
+errors <- c(fore_res_1$error1, fore_res_1$error2,
+           fore_res_5$error1,fore_res_5$error2,
+           fore_res_20$error1,fore_res_20$error2)
+
+my.result <- cbind(x.fact,errors)
+my.result <- as.data.frame(my.result)
+p <- ggplot(my.result, aes(x=factor(x.fact), 
+                           y=errors)) + geom_boxplot()
+p<- p + scale_color_grey() + theme_classic()
+print(p)
+
+#______________________________________________________________
+# t+1 error statistics
+fore_arima_1 <- fore_res_1$fore_model_1 #Arima forecasts
+fore_prophet_1 <- fore_res_1$fore_model_2    #Prophet forecasts
+actual_1 <- fore_res_1$actual    #Actual returns
+
+#Error statistics for t+1 forecasts
+error_statistics_arima_1 <- accuracy(fore_arima_1,actual_1)
+error_statistics_prophet_1 <- accuracy(fore_prophet_1,actual_1)
+
+#_______________________________________________________________________
+# t+5 error statistics
+fore_arima_5 <- fore_res_5$fore_model_1   #Arima forecasts
+fore_prophet_5 <- fore_res_5$fore_model_2  #Prophet forecasts
+actual_5 <- fore_res_5$actual  #Actual returns
+
+#Error statistics for t+5 forecasts
+error_statistics_arima_5 <- accuracy(fore_arima_5,actual_5)
+error_statistics_prophet_5 <- accuracy(fore_prophet_5,actual_5)
+
+#________________________________________________________________________
+# t+20 error statistics
+fore_arima_20 <- fore_res_20$fore_model_1 #Arima forecasts
+fore_prophet_20 <- fore_res_20$fore_model_2   #Prophet forecasts
+actual_20 <- fore_res_20$actual   #Actual returns
+
+#Error statistics for t+1 forecasts
+error_statistics_arima_20 <- accuracy(fore_arima_20,actual_20)
+error_statistics_prophet_20 <- accuracy(fore_prophet_20,actual_20)
+
+#_________________________________________________________________________
+
+
+#_____________________________________________________________________________________
+
+
+
+#-------- v) Diebold-Mariano Tests -------------------------------------------
+
+dm.test(fore_res_1$error1, fore_res_1$error2, h = 1)  #t+1 forecasts
+dm.test(fore_res_5$error1, fore_res_5$error2, h = 5)  #t+5 forecasts
+dm.test(fore_res_20$error1, fore_res_20$error2, h = 20)  #t+20 forecasts
+
+dm.test(fore_res_1$error1, fore_res_1$error2, h = 1, power = 1)  #t+1 forecasts
+dm.test(fore_res_5$error1, fore_res_5$error2, h = 5, power = 1)  #t+5 forecasts
+dm.test(fore_res_20$error1, fore_res_20$error2, h = 20, power = 1)  #t+20 forecasts
+
+dm.test(fore_res_1$error1, fore_res_1$error2, h = 1)  #t+1 forecasts
+dm.test(fore_res_5$error1, fore_res_5$error2, h = 5)  #t+5 forecasts
+dm.test(fore_res_20$error1, fore_res_20$error2, h = 20)  #t+20 forecasts
+
+
+
